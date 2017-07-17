bf4dbf85d76cc36b3545ecd4de86607dd2f1ca86
xy <xy@ali-186590d970d5.local>
2017-07-17 23:52:04 +0800

    MLP example

---
+++ MLP/sklearn_MLP_example.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+from sklearn.neural_network import MLPClassifier
+X = [[0.,0.],[1.,1.]]
+y=[0,1]
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5,2), random_state=1)
+clf.fit(X,y)
+clf.predict([[2.,2.],[-1.,-2.]])
+[coef.shape for coef in clf.coefs_]
+clf.predict_proba)[[2.,2.],[1.,2.]])
+clf.predict_proba([[2.,2.],[1.,2.]])
+print '----------------------'
+X = [[0.,0.],[1.,1.]]
+y= [[0,1],[1,1]]
+clf = MLPClassifier(solver='lbfgs',alpha=1e-5,hidden_layer_sizes=(15,),random_state=1)
+clf.fit(X,y)
+clf.predict([[1.,2.]])
+clf.predict([[0.,0.]])
